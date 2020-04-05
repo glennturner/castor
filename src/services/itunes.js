78c428520f1e7582c01b1a2fd69cff76132a0710
Glenn Turner <glenn@standalone.com>
2020-04-05 16:05:48 -0500

    Whitespace tweaks.

--- src/services/itunes.js
+++ src/services/itunes.js
@@ -1,11 +1,11 @@
 class Itunes {
   constructor() {
-    this._baseUrl = 'https://itunes.apple.com';
+    this._baseUrl = 'https://itunes.apple.com'
   }
 
   async search (query, opts = {}) {
-    const entity = 'podcast';
-    opts.explicit = opts.explicit === undefined ? true : opts.explicit;
+    const entity = 'podcast'
+    opts.explicit = opts.explicit === undefined ? true : opts.explicit
 
     const args = {
       attribute: 'titleTerm',
@@ -13,18 +13,18 @@ class Itunes {
       explicit: opts.explicit,
       media: entity,
       term: query
-    };
+    }
 
     const qs = Object.entries(args).map((arg) => {
-      arg[1] = encodeURIComponent(arg[1]);
+      arg[1] = encodeURIComponent(arg[1])
       return arg.join('=')
-    }).join('&');
+    }).join('&')
 
-    const searchUrl = this._baseUrl + '/search?' + qs;
-    console.log('SEARCH URL: ' + searchUrl);
+    const searchUrl = this._baseUrl + '/search?' + qs
+    console.log('SEARCH URL: ' + searchUrl)
 
     return fetch(searchUrl).then((response) => {
-      return response.json();
-    });
+      return response.json()
+    })
   }
 }
