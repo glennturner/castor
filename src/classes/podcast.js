f6d307bd9de6d93df75884329742ec3599711a0f
Glenn Turner <glenn@standalone.com>
2020-04-05 16:04:38 -0500

    Changed property assignments to be more readable. Added preliminary feed and class prop update handling.

--- src/classes/podcast.js
+++ src/classes/podcast.js
@@ -1,47 +1,102 @@
 class Podcast {
   constructor (args) {
-    [
-      'id', 'identity', 'artwork', 'title', 'feed'
-    ].map((key) => {
-      this[key] = args[key];
-    });
+    // Basic info
+    // Usually supplied by iTunes.
+    this._id = args.id
+    this._identity = args.identity
+    this._artwork = args.artwork
+    this._title = args.title
+    this._feed = args.feed
   }
 
-  showHTML () {
-    console.log( 'DOC?')
-    console.log(document.getElementsByTagName('body'))
-    return `
-      <div
-        class="podcast-show"
-      >
-        <img
-          src="${this.artwork}"
-        />
-        <div
-          class="title"
-        >
-          ${this.title}
-        </div>
-        <div
-          class="identity"
-        >
-          ${this.identity}
-        </div>
+  // Update podcast information via feed.
+  async _update () {
+    this.getFeed((parsed) => {
 
-        <div
-          class="capsule-player"
-        >
-          <span
-            class="btn btn-primary btn-play"
-          >
-            &#9657;
-          </span>
-          <span
-            class="btn btn-secondary btn-settings"
-          >
-            &#8943;
-        </div>
+    })
+  }
+
+  async getFeed () {
+    return fetch(this._feed)
+      .then(response => response.text())
+      .then((str) => {
+        this._parseFeed(
+          (new window.DOMParser()).parseFromString(str, "text/xml")
+        )
+      })
+  }
+
+  async playLatest () {
+    console.log('PLAY LATEST')
+    console.log(this)
+
+    await this._update()
+      .then(() => {
+      console.log('FEED')
+      console.log(this.episodes)
+    })
+  }
+
+  _parseFeed (xml) {
+    console.log('PARSE FEED')
+
+    let parsed = {
+      title: xml.querySelector('channel > title').textContent,
+      description: xml.querySelector('channel > description').textContent,
+      copyright: xml.querySelector('channel > copyright').textContent,
+      pubDate: xml.querySelector('channel > pubDate').textContent,
+      lastUpdated: xml.querySelector('channel > lastBuildDate').textContent,
+      artwork: xml.querySelector('channel > image > url').textContent,
+      episodesType: xml.getElementsByTagName('itunes:type')[0].textContent
+    }
+
+    console.log(parsed)
+  }
+
+  capsule () {
+    let showEle = document.createElement('div')
+    showEle.className = 'podcast-show'
+
+    showEle.innerHTML = `
+      <img
+        src="${this._artwork}"
+      />
+      <div
+        class="title"
+      >
+        ${this._title}
       </div>
-    `;
+      <div
+        class="identity"
+      >
+        ${this._identity}
+      </div>
+
+      <div
+        class="capsule-player"
+      >
+        <span
+          class="btn btn-primary btn-play"
+        >
+          &#9657;
+        </span>
+        <span
+          class="btn btn-secondary btn-settings"
+        >
+          &#8943;
+      </div>
+    `
+
+    console.log('SHOW ELE')
+    console.log(showEle.querySelectorAll('.btn-play'))
+
+    showEle.querySelectorAll('.btn-play')[0].addEventListener(
+      'click',
+      (e) => {
+        this.playLatest()
+      }
+    )
+
+    return showEle
   }
 }
