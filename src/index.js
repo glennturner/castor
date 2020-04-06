1127fb9f9eccf3321de831244c174ca18b8c4eb3
Glenn Turner <glenn@standalone.com>
2020-04-06 01:20:46 -0500

    Added http requirement.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const Store = require('electron-store')
+const http  = require('http')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
