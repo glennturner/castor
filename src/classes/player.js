a3f6b49cd0bffe8477bb0893cb929afae90377fd
Glenn Turner <glenn@standalone.com>
2020-04-06 01:19:40 -0500

    Added episode and playback support.

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,7 +1,12 @@
 class Player {
-  constructor (url, opts = {}) {
-    this._url = url
+  constructor (episode = undefined, opts = {}) {
+    this.episode = episode
+
     this._playing = false
+    this._playerId = 'player'
+    this._playerInterfaceId = 'player-ui'
+    this._player = document.getElementById(this._playerId)
+    this._currentTime = opts.currentTime || 0
   }
 
   get playing () {
@@ -12,7 +17,38 @@ class Player {
     this._playing = state ? true : false
   }
 
+  get episode () {
+    return this._episode
+  }
+
+  set episode (ep) {
+    this._episode = ep
+    player.load()
+  }
+
   play () {
+    console.log('PLAY!')
     this.playing = true
+
+    this._player.src = this._episode.episodeUrl
+    this._player.currentTime = this._currentTime
+    player.play()
+  }
+
+  pause () {
+    this.playing = false
+
+    let player = document.getElementById(this._playerId)
+    this._currentTime = player.currentTime
+    player.pause()
+
+  }
+
+  _setEvents () {
+    this._player.removeEventListener('play', this._onPlay)
+    this._player.removeEventListener('pause', this._onPause)
+
+    this._player.addEventListener('play', this._onPlay)
+    this._player.addEventListener('pause', this._onPause)
   }
 }
