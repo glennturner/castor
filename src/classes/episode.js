446247a2cbfa132ec9f30e58ea4b3a1eaedd4ea3
Glenn Turner <glenn@standalone.com>
2020-04-10 20:34:27 -0500

    WIP: construction work.

--- src/classes/episode.js
+++ src/classes/episode.js
@@ -1,8 +1,17 @@
 class Episode {
-  #podcast
-
   constructor (podcast, args = {}) {
-    this.#podcast = podcast
+    this.podcast = podcast
+
+    this.author = args.author
+    this.description = args.description
+    this.duration = args.duration
+    this.episodeNum = args.episodeNum
+    this.episodeUrl = args.episodeUrl
+    this.id = args.id
+    this.link = args.link
+    this.podcast = args.podcast
+    this.pubDate = args.pubDate
+    this.title = args.title
   }
 }
 
