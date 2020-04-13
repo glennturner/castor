9a3c76b5fb6a51d8c28a283778824e36807b5dad
Glenn Turner <glenn@standalone.com>
2020-04-12 20:21:32 -0500

    Added getEpisodeById. Added feed storage suport. Fleshed out update handling. Added episode event handling. Improved private props.

--- src/classes/podcast.js
+++ src/classes/podcast.js
@@ -1,48 +1,29 @@
 class Podcast {
-  #artwork
-  #author
-  #description
-  #copyright
-  #episodesType
   #feed
-  #id
-  #lastUpdated
   #player
-  #pubDate
-  #title
 
   constructor (args) {
-    console.log('ITEM: ' + args.identity)
     // Basic info
     // Usually supplied by iTunes.
-    this.#id = args.id
-    this.#author = args.identity
-    this.#artwork = args.artwork
+
+    // This is usually the iTMS podcast collectionId.
+    this.id = args.id
+
+    /*
+      Pre-populate?
+    */
+
+    this.author = args.identity
+    this.artwork = args.artwork
+    this.copyright = args.copyright
     this.#feed = args.feed
-    this.#title = args.title
-    this.#player = new Player
+    this.link = args.link
+    this.title = args.title
+    this.pubDate = args.pubDate
+    this.episodesType = args.episodesType
     this.episodes = args.episodes || []
-  }
 
-  // Update podcast information via feed.
-  async _update (forceUpdate = false) {
-    // Don't update if already updated.
-    if (this.#lastUpdated && !forceUpdate) {
-      return
-    }
-
-    return await this.getFeed().then((parsed) => {
-      this.#title = parsed.title
-      this.#author = parsed.author
-      this.#description = parsed.description
-      this.#copyright = parsed.copyright
-      this.#pubDate = parsed.pubDate
-      this.#lastUpdated = parsed.lastUpdated || new Date
-      this.#artwork = parsed.artwork
-      this.#episodesType = parsed.episodesType
-
-      this.episodes = parsed.episodes
-    })
+    this.#player = new Player
   }
 
   async getFeed () {
@@ -51,18 +32,21 @@ class Podcast {
 
   async playLatest () {
     return await this._update().then(() => {
-      let ep = this.episodes[0]
-      ep.episodeUrl = 'http://localhost:5000/podcast'
-
-      // Not 100% sure why `Player#togglePlayback` is not working here,
-      // so we manually control pausing.
-      if ( this.#player.playing && this.#player.episode == ep ) {
-        this.#player.pause()
-      } else {
-        this.#player.episode = ep
-        this.#player.play()
-      }
+      this.playEpisode(this.episodes[0])
     })
+  }
+
+  async playEpisode (ep) {
+    // ep.episodeUrl = 'http://localhost:5000/podcast'
+
+    // Not 100% sure why `Player#togglePlayback` is not working here,
+    // so we manually control pausing.
+    if ( this.#player.playing && this.#player.episode.episodeUrl == ep.episodeUrl ) {
+      this.#player.pause()
+    } else {
+      this.#player.episode = ep
+      this.#player.play()
+    }
   }
 
   capsule () {
@@ -71,17 +55,17 @@ class Podcast {
 
     showEle.innerHTML = `
       <img
-        src="${this.#artwork}"
+        src="${this.artwork}"
       />
       <div
         class="title"
       >
-        ${this.#title}
+        ${this.title}
       </div>
       <div
         class="author"
       >
-        ${this.#author}
+        ${this.author}
       </div>
 
       <div
@@ -146,7 +130,7 @@ class Podcast {
               class="podcast-show-artwork"
             >
               <img
-                src="${this.#artwork}"
+                src="${this.artwork}"
                 class="artwork"
               />
             </div>
@@ -154,15 +138,29 @@ class Podcast {
               class="podcast-show-content"
             >
               <h2>
-                ${this.#title}
+                ${this.title}
               </h2>
               <h3>
-                ${this.#author}
+                ${this.author}
               </h3>
         ` + eps + `
             </div>
           </div>
         `
+
+        detailedEle.querySelectorAll('.episode').forEach(ele => {
+          ele.addEventListener(
+            'click',
+            (e) => {
+              let id = e.currentTarget.dataset.episodeId
+              console.log('ELE: ' + id)
+              console.log(e.currentTarget)
+              let ep = this._getEpisodeById(id)
+              console.log(ep)
+              this.playEpisode(ep)
+            }
+          )
+        })
 
         return detailedEle
       })
@@ -174,6 +172,7 @@ class Podcast {
     return `
       <div
         class="episode"
+        data-episode-id="${ep.id}"
       >
         <h4>
           ${ep.episodeNum} ${ep.title}
@@ -183,5 +182,66 @@ class Podcast {
         </p>
       </div>
     `
+  }
+
+  _getEpisodeById (id) {
+    return this.episodes.filter(ep => ep.id === id)[0]
+  }
+
+  _json () {
+    return JSON.stringify(
+      {
+        title: this.title,
+        author: this.author,
+        description: this.description,
+        copyright: this.copyright,
+        pubDate: this.pubDate,
+        lastUpdated: this.lastUpdated,
+        link: this.link,
+        artwork: this.artwork,
+        episodesType: this.episodesType,
+        lastRetrieved: this.lastRetrieved,
+        episodes: this.episodes//.map(ep => ep.obj)
+      }
+    )
+  }
+
+  _shouldUpdate () {
+    let now = new Date
+
+    return true
+  }
+
+  _storeFeed () {
+    localStorage.setItem(
+      this.id, this._json()
+    )
+  }
+
+  // Update podcast information via feed.
+  async _update (forceUpdate = false) {
+    // Don't update if already updated.
+    if (this._shouldUpdate() && forceUpdate) {
+      return
+    }
+
+    return await this.getFeed().then((parsed) => {
+      this.title = parsed.title
+      this.author = parsed.author
+      this.description = parsed.description
+      this.copyright = parsed.copyright
+      this.pubDate = parsed.pubDate
+      this.lastUpdated = parsed.lastUpdated || new Date
+      this.link = parsed.link
+      this.artwork = parsed.artwork
+      this.episodesType = parsed.episodesType
+      this.lastRetrieved = new Date
+
+      this.episodes = parsed.episodes.map((ep) => {
+        return new Episode(this.id, ep)
+      })
+
+      this._storeFeed()
+    })
   }
 }
