e95ce2904fac91ba8c8927fb8e040525f50f7a40
Glenn Turner <glenn@standalone.com>
2020-04-12 20:18:19 -0500

    Added episodeNum default. Added obj and json methods.

--- src/classes/episode.js
+++ src/classes/episode.js
@@ -1,17 +1,38 @@
 class Episode {
-  constructor (podcast, args = {}) {
-    this.podcast = podcast
+  constructor (podcastId, args = {}) {
+    this.podcastId = podcastId
 
     this.author = args.author
     this.description = args.description
     this.duration = args.duration
-    this.episodeNum = args.episodeNum
+    this.episodeNum = args.episodeNum || ''
     this.episodeUrl = args.episodeUrl
     this.id = args.id
     this.link = args.link
     this.podcast = args.podcast
     this.pubDate = args.pubDate
     this.title = args.title
+
+    this.playing = false
+  }
+
+  json () {
+    return JSON.stringify(this.obj)
+  }
+
+  obj () {
+    return {
+      author: this.author,
+      description: this.description,
+      duration: this.duration,
+      episodeNum: this.episodeNum,
+      episodeUrl: this.episodeUrl,
+      id: this.id,
+      link: this.link,
+      podcast: this.podcast.guid,
+      pubDate: this.pubDate,
+      title: this.title
+    }
   }
 }
 
