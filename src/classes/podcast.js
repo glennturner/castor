63549dd225a680a47bc3ae5f7ba6ff7e4f94bd1b
Glenn Turner <glenn@standalone.com>
2020-04-14 22:03:02 -0500

    WIP: state handling. Sub/unsub/archive/unarchive work.

--- src/classes/podcast.js
+++ src/classes/podcast.js
@@ -1,10 +1,14 @@
 class Podcast {
-  #feed
   #player
+  #savedKey = 'C-SAVED'
+  #stateKey
+  #stateKeyPrefix = 'C-P-'
+  #subscribedKey = 'C-SUBS'
 
   constructor (args) {
     // This is usually the iTMS podcast collectionId.
     this.id = args.id
+    this.#stateKey = this.#stateKeyPrefix + this.id
 
     /*
     // Basic info
@@ -16,7 +20,7 @@ class Podcast {
     this.author = args.identity
     this.artwork = args.artwork
     this.copyright = args.copyright
-    this.#feed = args.feed
+    this.feed = args.feed
     this.link = args.link
     this.title = args.title
     this.pubDate = args.pubDate
@@ -26,8 +30,52 @@ class Podcast {
     this.#player = new Player
   }
 
+  get state () {
+    return localStorage.getItem(this.#stateKey) || {
+      episodes: {}
+    }
+  }
+
+  set state (state) {
+    localStorage.setItem(
+      this.id, state
+    )
+  }
+
+  subscribe () {
+    user.subscribe(this.id)
+
+    this._showDetailedView()
+  }
+
+  unsubscribe () {
+    user.unsubscribe(this.id)
+
+    this._showDetailedView()
+  }
+
+  subscribed () {
+    return user.subscribedPodcasts.includes(this.id)
+  }
+
+  archive () {
+    user.archive(this.id)
+
+    this._showDetailedView()
+  }
+
+  unarchive () {
+    user.unarchive(this.id)
+
+    this._showDetailedView()
+  }
+
+  archived () {
+    return true
+  }
+
   async getFeed () {
-    return new Feed(this.#feed).get()
+    return new Feed(this.feed).get()
   }
 
   async playLatest () {
@@ -42,7 +90,7 @@ class Podcast {
 
     // Not 100% sure why `Player#togglePlayback` is not working here,
     // so we manually control pausing.
-    if ( this.#player.playing && this.#player.episode.episodeUrl == ep.episodeUrl ) {
+    if (this.#player.playing && this.#player.episode.episodeUrl == ep.episodeUrl) {
       this.#player.pause()
     } else {
       this.#player.episode = ep
@@ -102,17 +150,7 @@ class Podcast {
 
     showEle.addEventListener(
       'click',
-      (e) => {
-        this.detailed()
-          .then((ele) => {
-            view.change('podcast',
-              ele,
-              {
-                podcastId: this.id
-              }
-            )
-          })
-      }
+      (e) => this._showDetailedView
     )
 
     return showEle
@@ -149,10 +187,66 @@ class Podcast {
               <h3>
                 ${this.author}
               </h3>
+              <nav
+                class="podcast-options"
+              >
+                <button
+                  id="podcast-subscribe-toggle"
+                  type="button"
+                  class="btn btn-sm btn-primary"
+                  data-toggle="modal"
+                  data-target="#podcast-options"
+                >
+                  ${this.subscribed() ? 'Unsubscribe' : 'Subscribe'}
+                </button>
+
+                <button
+                  id="podcast-options-btn"
+                  type="button"
+                  class="btn btn-sm btn-primary"
+                  data-toggle="modal"
+                  data-target="#podcast-options"
+                >
+                  Options
+                </button>
+
+                <!-- Modal -->
+                <dialog id="podcastOptions">
+                  <form method="dialog">
+                    <menu>
+                      <button
+                        class="btn btn-danger"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        id="savePodcastOptions"
+                        class="btn btn-primary"
+                      >
+                        Confirm
+                      </button>
+                    </menu>
+                  </form>
+                </dialog>
+              </nav>
+              <div
+                class="episodes"
+              >
         ` + eps + `
+              </div>
             </div>
           </div>
         `
+
+        detailedEle.querySelector('#podcast-subscribe-toggle').addEventListener('click', () => {
+          this.subscribed() ? this.unsubscribe() : this.subscribe()
+        })
+
+        detailedEle.querySelector('#podcast-options-btn').addEventListener('click', () => {
+          podcastOptions.showModal()
+        })
+
+        console.log('subscribed? ' + this.subscribed())
 
         detailedEle.querySelectorAll('.episode').forEach(ele => {
           ele.addEventListener(
@@ -211,7 +305,7 @@ class Podcast {
       {
         id: this.id,
         title: this.title,
-        feed: this.#feed,
+        feed: this.feed,
         author: this.author,
         description: this.description,
         copyright: this.copyright,
@@ -221,7 +315,8 @@ class Podcast {
         artwork: this.artwork,
         episodesType: this.episodesType,
         lastRetrieved: this.lastRetrieved,
-        episodes: this.episodes//.map(ep => ep.obj)
+        state: this.state,
+        episodes: this.episodes
       }
     )
   }
@@ -232,10 +327,20 @@ class Podcast {
     return true
   }
 
+  _showDetailedView () {
+    this.detailed()
+      .then((ele) => {
+        view.change('podcast',
+          ele,
+          {
+            podcastId: this.id
+          }
+        )
+      })
+  }
+
   _storeFeed () {
-    localStorage.setItem(
-      this.id, this._json()
-    )
+    this.state = this._json()
   }
 
   // Update podcast information via feed.
