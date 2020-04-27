a2b063da5b618b8539855620b012e72cbb5f1a1b
Glenn Turner <glenn@standalone.com>
2020-04-27 01:43:43 -0500

    Changed played badge to light. Added pubDisplayDate.

--- src/classes/episode.js
+++ src/classes/episode.js
@@ -113,7 +113,7 @@ class Episode {
     if (this.played) {
       playedBadge = `
         <span
-          class="badge badge badge-secondary"
+          class="badge badge badge-light"
         >
           <svg class="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
@@ -200,6 +200,12 @@ class Episode {
 
   isActive () {
     return this.id === player.state.episodeId
+  }
+
+  pubDisplayDate () {
+    let eles = this.pubDate.split(' ')
+
+    return [ eles[2], (eles[1] + ','), eles[3] ].join(' ')
   }
 }
 
