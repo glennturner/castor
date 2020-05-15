602de975269702aa2196f982fe7ad2fb6961db7c
Glenn Turner <glenn@standalone.com>
2020-05-15 01:12:46 -0500

    Added reset and togglePlay handling.

--- src/preload.js
+++ src/preload.js
@@ -1,35 +1,57 @@
 const {
   contextBridge,
   ipcRenderer
-} = require("electron");
+} = require('electron')
+
+const Podcast = require('./classes/podcast')
+const Player = require('./classes/player')
+const User = require('./classes/user')
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
-  "api", {
+  'api', {
     send: (channel, data) => {
       // whitelist channels
       let validChannels = [
         'hash',
+        'reset',
         'saveEpisode',
         'parseXML'
-      ];
+      ]
 
       if (validChannels.includes(channel)) {
-        ipcRenderer.send(channel, data);
+        ipcRenderer.send(channel, data)
       }
     },
     receive: (channel, func) => {
       let validChannels = [
         'hashed',
+        'resetCompleted',
         'episodeSaved',
         'parsedXML'
-      ];
+      ]
 
       if (validChannels.includes(channel)) {
         // Deliberately strip event as it includes `sender`
-        ipcRenderer.on(channel, (event, ...args) => func(...args));
+        ipcRenderer.on(channel, (event, ...args) => func(...args))
       }
     }
   }
 );
+
+ipcRenderer.on('reset', (args) => {
+  User.reset()
+})
+
+ipcRenderer.on('togglePlay', () => {
+  // Encountering loading issues with Player (which relies on Podcast)
+  // so we manually invoke play/pause here via the audio element.
+  let audioPlayer = document.getElementById('player')
+
+  if (audioPlayer.paused) {
+    audioPlayer.play()
+  } else {
+    audioPlayer.pause()
+  }
+})
