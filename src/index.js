18c26476b1e194bc3b2c4950e65aa153ad8ac6ef
Glenn Turner <glenn@standalone.com>
2020-05-27 05:34:46 -0500

    Removed auto-console display.

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const createWindow = () => {
     }
   })
 
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'))
