c8dfb69dd807e71a4cb11517ed8c554123fab3f9
Glenn Turner <glenn@standalone.com>
2020-06-28 20:56:17 -0500

    Fixed bad descrEle assignment handling.

--- src/classes/feed.js
+++ src/classes/feed.js
@@ -57,7 +57,7 @@ class Feed {
 
       // Sigh. Default to iTunes, since something changed with with a bunch of feeds there.
       let itunesSumm = item.getElementsByTagName('itunes:summary')
-      let descrEle  = itunesSumm ? itunesSumm[0] : item.querySelector('description')
+      let descrEle  = itunesSumm.length ? itunesSumm[0] : item.querySelector('description')
 
       parsed.episodes.push(
         {
