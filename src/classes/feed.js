9ff269751e7ac562b34813bd62b1b0f0eccdecb8
Glenn Turner <glenn@standalone.com>
2020-08-26 15:19:34 -0500

    Improved error handling.

--- src/classes/feed.js
+++ src/classes/feed.js
@@ -8,17 +8,31 @@ class Feed {
 
   async get () {
     return await this.getXML().then((str) => {
+      if (!str) { return }
+
       this.#xml = (new window.DOMParser()).parseFromString(str, "text/xml")
 
       return this.parse()
+    }).catch(err => {
+      console.error('An error occurred with this podcast:')
+      console.error(err)
     })
   }
 
   async getXML () {
     return await fetch(this.#path)
-      .then(response => response.text())
+      .then(response => {
+        if (!response.ok) {
+          throw response
+        } else {
+          return response.text()
+        }
+      })
       .then((str) => {
         return str
+      }).catch(err => {
+        console.error('An error occurred downloading this podcast:')
+        console.error(err)
       })
   }
 
